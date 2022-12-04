db0a7336bb789f08aa52c0767fa4f7703b6f3812
Khawor <thomas.renoullin@gmail.com>
2022-12-04 13:34:44 +0100

    Update aoc2.py

--- Khawor/04/aoc2.py
+++ Khawor/04/aoc2.py
@@ -7,7 +7,8 @@ with open('input.csv', newline='') as f:
     for row in reader:
         range1 = row[0].split('-')
         range2 = row[1].split('-')
-        if (int(range1[0]) <= int(range2[0]) and int(range1[1]) >= int(range2[0])) or (int(range2[0]) <= int(range1[1]) and int(range2[1]) >= int(range1[1])) or (int(range2[0]) <= int(range1[0]) and int(range2[1]) >= int(range1[0])) or (int(range1[0]) <= int(range2[1]) and int(range1[1]) >= int(range2[1])):
+        if (int(range1[0]) <= int(range2[0]) and int(range1[1]) >= int(range2[0])) or (int(range2[0]) <= int(range1[1]) and int(range2[1]) >= int(range1[1])) or \
+            (int(range2[0]) <= int(range1[0]) and int(range2[1]) >= int(range1[0])) or (int(range1[0]) <= int(range2[1]) and int(range1[1]) >= int(range2[1])):
             print(str(range1)+" - "+str(range2))
             tot+=1
     
