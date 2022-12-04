b7a1616dff677f21e2fe7ac06d9df932f81fdcd0
KapyBoiveau <q.boiveau@gmail.com>
2022-12-04 16:03:23 +0100

    p2 v2

---
+++ Kapy/day04/day4_p2v2.py
@@ -0,0 +1,20 @@
+
+nb = 0
+with open('./Kapy/day04/input.txt', 'r') as f:
+    for line in f.readlines():
+        pair = line.strip()
+
+        a, b = pair.split(',')
+        a = a.split('-')
+        b = b.split('-')
+        
+        a[1] = str(int(a[1])+1) # ['2', '4'] --> ['2', '5']
+        b[1] = str(int(b[1])+1)
+
+        setA = set(range(*map(int, a)))
+        setB = set(range(*map(int, b)))
+
+        if (setA & setB):
+            nb += 1
+
+print(nb)
