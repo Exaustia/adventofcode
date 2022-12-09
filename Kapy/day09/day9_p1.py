c0b66a93b463feae7ed8494dbd08c508425e135c
KapyBoiveau <q.boiveau@gmail.com>
2022-12-09 13:53:09 +0100

    small fixes and comments

--- Kapy/day09/day9_p1.py
+++ Kapy/day09/day9_p1.py
@@ -1,25 +1,22 @@
 
 import numpy as np
 
+res = set()
 
 h = np.array([0, 0])
 t = np.array([0, 0])
-res = set()
 
 def follow(h, t):
     toGo = h - t
     toGoAbs = np.array(list(map(abs, toGo)))
     toGoSigns = np.array([0, 0])
-    for i, x in enumerate(toGo):
-        if x == 0:
-            toGoSigns[i] = 0
-        else:
+    for i, x in enumerate(toGoAbs):
+        if x != 0:
             toGoSigns[i] = int(toGo[i] / toGoAbs[i])
 
-    if [pos for pos in toGoAbs if pos > 1]:
-        idxMax = np.where(toGoAbs == max(toGoAbs))[0][0]
+    if 2 in toGoAbs: 
+        idxMax = np.where(toGoAbs == max(toGoAbs))[0][0] # look for the 2
         toGoAbs[idxMax] = toGoAbs[idxMax]-1
-        
         toGo = toGoAbs * toGoSigns
         return t + toGo
     else:
@@ -28,8 +25,8 @@ def follow(h, t):
 with open('./Kapy/day09/input.txt', 'r') as f:
     for line in f.readlines():
         dir, nb = line.strip().split(' ')
-        for _ in range(0, nb):
-
+        for _ in range(0, int(nb)):
+            # move the Head
             if dir == 'R':
                 h[0] = h[0] + 1
             elif dir == 'L':
@@ -38,7 +35,7 @@ with open('./Kapy/day09/input.txt', 'r') as f:
                 h[1] = h[1] + 1
             elif dir == 'D':
                 h[1] = h[1] - 1
-            t = follow(h, t)
+            t = follow(h, t) # move the Tail
             
             res.add(str(t))
 
--- Kapy/day09/day9_p2.py
+++ Kapy/day09/day9_p2.py
@@ -1,45 +1,42 @@
 
 import numpy as np
 
+res = set()
 rope = []
 for _ in range(0,10):
     rope.append(np.array([0, 0]))
-res = set()
 
 def follow(h, t):
     toGo = h - t
     toGoAbs = np.array(list(map(abs, toGo)))
     toGoSigns = np.array([0, 0])
-    for i, x in enumerate(toGo):
-        if x == 0:
-            toGoSigns[i] = 0
-        else:
-            toGoSigns[i] = int(toGo[i] / toGoAbs[i])
+    for i, x in enumerate(toGoAbs):
+        if x != 0:
+            toGoSigns[i] = int(toGo[i] / toGoAbs[i]) # [5, -7] --> [1, -1]
 
-    if [pos for pos in toGoAbs if pos > 1]:
-        idsMax = np.where(toGoAbs == max(toGoAbs))[0]
-        for idMax in idsMax:
+    if 2 in toGoAbs: 
+        for idMax in np.where(toGoAbs == 2)[0]: # look for all 2s
             toGoAbs[idMax] = toGoAbs[idMax]-1
         toGo = toGoAbs * toGoSigns
-        return t + toGo
+        return t + toGo        
     else:
         return t # don't move
 
 with open('./Kapy/day09/input.txt', 'r') as f:
     for line in f.readlines():
         dir, nb = line.strip().split(' ')
-        for _ in range(0, nb):
-            h = rope[0]
+        for _ in range(0, int(nb)):
+            # move the Head
             if dir == 'R':
-                h[0] = h[0] + 1
+                rope[0][0] = rope[0][0] + 1
             elif dir == 'L':
-                h[0] = h[0] - 1
+                rope[0][0] = rope[0][0] - 1
             elif dir == 'U':
-                h[1] = h[1] + 1
+                rope[0][1] = rope[0][1] + 1
             elif dir == 'D':
-                h[1] = h[1] - 1
-            for i in range(0,9):
-                rope[i] = follow(rope[i], rope[i+1])
+                rope[0][1] = rope[0][1] - 1
+            for i in range(1,10): # move each part of the body
+                rope[i] = follow(rope[i-1], rope[i]) # the current part in the Tail, the previous one is the Head
             res.add(str(list(rope[9])))
 
 print(len(res))
