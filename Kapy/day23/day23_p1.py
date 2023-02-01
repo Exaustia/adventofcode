f6aa26ecefe058fad478f7a8dce448215d4830ab
KapyBoiveau <q.boiveau@gmail.com>
2023-02-01 17:42:51 +0100

    day 23

--- Kapy/day23/day23_p1.py
+++ Kapy/day23/day23_p1.py
@@ -21,6 +21,14 @@ for y, line in enumerate(open('./Kapy/day23/inputTest.txt', 'r').read().split('\
             elves.append((x,y))
 elves = np.array(elves)
 
+def draw():
+    elvesList = [tuple(x) for x in elves]
+    for y in range(6):
+        line = ''
+        for x in range(5):
+            line += '#' if (x, y) in elvesList else '.'
+        print(line)
+
 prios = [(0, -1), (1, -1), (-1, -1), # N NE NW
          (0, 1), (1, 1), (-1, 1), # S SE SW
          (-1, 0), (-1, -1), (-1, 1), # W NW SW
@@ -28,6 +36,7 @@ prios = [(0, -1), (1, -1), (-1, -1), # N NE NW
 
 while True:
     propos = []
+    draw()
     for elf in elves:
         moved = False
         if someoneAround(elf):
@@ -42,6 +51,7 @@ while True:
     if set([tuple(x) for x in elves]) == set([tuple(x) for x in propos]): # no one moved
         break
     else:
+        elves = propos
         prios = prios[3:] + prios[:3]
 
-print(elves)
+draw()
